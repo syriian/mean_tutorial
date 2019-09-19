34f948a12bcf1f011f67d56cafa57b717b6a752b
Syrian Saliba <syriangabrielsaliba@gmail.com>
2019-09-20 01:03:45 +0200

    added form for input fields and validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 
+import { Post } from "../post.model";
+import { NgForm } from "@angular/forms";
+
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
@@ -8,16 +11,21 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class PostCreateComponent {
   enteredContent = "";
   enteredTitle = "";
-  //---------outputs data to parent via the eventemitter
-  @Output() postCreated = new EventEmitter();
+  //---------outputs data to parent via the eventemitter of generic type post
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post = {
-      //---setting data when passed from view.
-      title: this.enteredTitle,
-      content: this.enteredContent
+  //---Passing in form of type NgForm
+  onAddPost(form: NgForm) {
+    //--validate form
+    if (form.invalid) {
+      return; // do nothing
+    }
+    const post: Post = {
+      //---setting data when passed from view via the form object parameter.
+      title: form.value.title,
+      content: form.value.content
     };
-    //------Calling the emit function to emit postCreated variable
+    //----Calling the emit function with param of type post
     this.postCreated.emit(post);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from "@angular/core";
 
+import { Post } from "../post.model";
+
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
