b2880d740911238010a9fd9ade5e06c15f635787
Syrian Saliba <syriangabrielsaliba@gmail.com>
2019-10-02 15:16:39 +0200

    form control for image, added div that shows uploaded image and som styling

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -17,6 +17,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup; // groups all parts of a form.
+  imagePreview: string;
   private mode = "create";
   private postId: string;
   //---------outputs data to parent via the eventemitter of generic type post
@@ -33,7 +34,8 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      content: new FormControl(null, { validators: [Validators.required] })
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] })
     });
     //paramMap is a observable, observs for changes in the url
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -61,6 +63,18 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
+    //convert data to normal url
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = "reader.result";
+    };
+    reader.readAsDataURL(file);
   }
 
   //---Passing in form of type NgForm
